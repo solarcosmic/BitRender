0126127ef921aa99e8cc744c8fe0edf35ea89758
Solar <abe@solarcosmic.net>
2025-08-09 18:37:28 +1200

    Attempt to add MySQL db

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const kleur = require("kleur");
 const swaggerUi = require("swagger-ui-express"); // for later
+const mysql = require("mysql");
 const app = express();
 app.use(express.json());
 
+var sql = mysql.createConnection({
+    host: "",
+    user: "",
+    password: "",
+});
+
+sql.connect(function(err) {
+    if (err) return console.log(kleur.red("[BitRender] Database error occurred - " + err));
+    console.log("Connected!");
+})
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     log(`
