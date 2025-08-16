8e98b9d318518282ea7506b946825d70e3a6e86f
Solar <abe@solarcosmic.net>
2025-08-16 20:53:32 +1200

    Add /images, /images/:id/rename, and more tweaks

--- index.js
+++ index.js
@@ -8,12 +8,9 @@ const mysql = require("mysql");
 const sharp = require("sharp");
 const yaml = require("js-yaml");
 const fs = require("fs");
+const sanitize = require("sanitize-filename");
 const app = express();
 app.use(express.json({limit: "3mb"}));
-
-var db = "bitrender";
-var host = "127.0.0.1";
-var user = "root";
 
 var settings = {}
 const start = Date.now();
@@ -27,6 +24,8 @@ try {
     return process.exit();
 }
 welcomePrint();
+
+const port = settings?.general?.port || 3000;
 
 var sql = mysql.createConnection({
     host: settings?.database?.host || "127.0.0.1",
@@ -72,7 +71,7 @@ sql.connect(function(err) {
         console.log(kleur.red("Database error occurred - " + err));
         return process.exit();
     }
-    console.log(kleur.green("✱  Connected to database! - " + db + "@" + host + " as " + user));
+    console.log(kleur.green("✱  Connected to database! - " + settings?.database?.db_name + "@" + settings?.database?.host + " as " + settings?.database?.user));
     app.listen(port, () => {
         console.log("✱  Listening on " + kleur.underline("127.0.0.1:" + port) + " | Press Ctrl+C / Cmd+C to exit (^C)");
         console.log("Took approximately " + (Date.now() - start) + "ms to load.");
@@ -103,6 +102,8 @@ function isBase64(str) {
  *     description: |
  *       Converts a Base64 image string into a different format (jpg, png, webp, ...).
  * 
+ *       You can call [**GET /images/formats**](#/default/get_images_formats) to return a list of formats that are officially supported, but it may also be possible to use the formats [Sharp](https://sharp.pixelplumbing.com/api-output/) supports.
+ * 
  *       **NOTE:** On Swagger UI, the Base64 response may be trimmed. Please consider using the API directly or a frontend implementation.
  *     responses:
  *       200:
@@ -129,13 +130,14 @@ app.post("/images/convert/base64", async (req, res) => {
     try {
         console.log(req.body);
         if (!req?.body?.format) throw Error("No format was provided! Please use the `format` tag.");
-        if (!isBase64(req.body.format)) throw Error("Format is not valid base64!");
+        if (!isBase64(req.body.base64)) throw Error("Base64 string is not valid!");
         const bufferSplit = req.body.base64.split(";base64,");
         //const previousFormat = bufferSplit.shift(); // e.g. data:image/png
         const previousBase64 = bufferSplit.pop(); // the string of letters after the content type
         var newFormat = null;
         const buffer = Buffer.from(previousBase64, "base64");
-        const format = req?.body?.format || "jpeg";
+        const format = req?.body?.format.toLowerCase() || "jpeg";
+        if (!base64types[format]) throw Error("Invalid format!");
         const grayscale = await sharp(buffer).toFormat(format).toBuffer();
         for (const [key, value] of Object.entries(base64types)) {
             if (value.includes(format)) {
@@ -161,7 +163,7 @@ app.post("/images/convert/base64", async (req, res) => {
  * /images/convert/upload:
  *   post:
  *     description: |
- *       Allows you to upload an image file and convert it into a different format (jpg, png, webp, ...). Returns Base64 version.
+ *       Allows you to upload an image file and convert it into a different format (jpg, png, webp, ...). Returns the image in Base64.
  * 
  *       **NOTE:** On Swagger UI, the Base64 response may be trimmed. Please consider using the API directly or a frontend implementation.
  *     responses:
@@ -232,7 +234,7 @@ app.post("/images/upload", uplMulter.single("file"), async (req, res) => {
         const format = req.file.mimetype.split("/")[1];
         const basePrefix = base64types[format] || "data:image/" + format + ";base64,";
         const base64 = basePrefix + req.file.buffer.toString("base64");
-        sql.query("INSERT INTO images (filename, format, data) VALUES (?, ?, ?)", [sanitize(req.file.originalname), req.file.format, base64], (err, result) => {
+        sql.query("INSERT INTO images (filename, format, data) VALUES (?, ?, ?)", [sanitize(req.file.originalname), req.file.mimetype.split("/")[1], base64], (err, result) => {
             res.send({
                 success: true,
                 id: result?.insertId
@@ -418,8 +420,8 @@ app.get("/images/:id/raw", async (req, res) => {
  *     
  */
 app.get("/images/:id", async (req, res) => {
-    if (!req.params.id) throw Error("No valid image ID!");
     try {
+        if (!req.params.id) throw Error("No valid image ID!");
         getBase64FromID(req.params.id, (err, result) => {
             if (err) return res.status(400).send({success: false, error: err.toString()});
             res.send({success: true, id: req.params.id, format: result.format, base64: result.pureBase64.toString()});
@@ -430,8 +432,117 @@ app.get("/images/:id", async (req, res) => {
     };
 })
 
+/**
+ * @openapi
+ * /images/{id}/rename:
+ *   put:
+ *     description: Renames an image in the database.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The ID of the image you want to fetch.
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name you want to change the image's file name to.
+ *     responses:
+ *       200:
+ *         description: Returns the image from the database in Base64 format.
+ *       400:
+ *         description: Requirement not met or client/server side error (e.g. ID not found).
+ *     
+ */
+app.put("/images/:id/rename", async (req, res) => {
+    try {
+        if (!req?.params?.id) throw Error("No valid image ID!");
+        if (!req?.body?.name) throw Error("Please specify a name you want to rename the image to!");
+        sql.query("SELECT * FROM images WHERE id = ?", [req.params.id], async (err, resp) => {
+            if (err) throw Error("Database error. Please contact an admin for more help.");
+            var name = sanitize(req.body.name);
+            var prev_name = resp[0].filename;
+            sql.query("UPDATE images SET filename = ?", [name], async (err, resp) => {
+                if (err) throw Error("Database error while updating name. Please contact an admin for more help.");
+                res.send({success: true, previous: prev_name, new: name})
+            })
+        });
+    } catch (e) {
+        res.status(400).send({success: false, error: e.toString() || "No valid error was provided!"});
+        console.log(kleur.red("[BitRender] Error on /images/:id/rename - " + e));
+    }
+})
+
+/**
+ * @openapi
+ * /images:
+ *   get:
+ *     description: |
+ *       Returns an array of all the images stored in the database.
+ * 
+ *       URL queries are supported, for example:
+ *       /images?limit=5&offset=7&base64=true
+ *     parameters:
+ *       - name: limit
+ *         in: query
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: Maximum amount of images to be returned.
+ *       - name: offset
+ *         in: query
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         description: Amount of images to skip from the beginning.
+ *       - name: base64
+ *         in: query
+ *         required: false
+ *         schema:
+ *           type: boolean
+ *         description: If true, includes the Base64 along with the images.
+ *     responses:
+ *       200:
+ *         description: Returns the image from the database in Base64 format.
+ *       400:
+ *         description: Requirement not met or client/server side error (e.g. ID not found).
+ *     
+ */
+app.get("/images", async (req, res) => {
+    try {
+        const limit = parseInt(req.query.limit) || 10;
+        const offset = parseInt(req.query.offset) || 0;
+        sql.query("SELECT * FROM images LIMIT ? OFFSET ?", [limit, offset], async (err, resp) => {
+            const result = [];
+            for (const item of resp) {
+                const thing = {
+                    id: item?.id,
+                    filename: item?.filename,
+                    format: item?.format,
+                    created_at: item?.created_at
+                };
+                if (req?.query?.base64) {
+                    if (req?.query?.base64.toLowerCase() == "true") thing["base64"] = "data:image/" + item?.format + ";base64," + item?.data.toString("base64");
+                }
+                result.push(thing);
+            };
+            res.send({success: true, limit, offset, images: result});
+        });
+    } catch (e) {
+        res.status(500).send({success: false, error: e.toString() || "No valid error was provided!"});
+        console.log(kleur.red("[BitRender] Error on /images/:id/rename - " + e));
+    }
+})
+
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(openApiSpecification, {explorer: true}));
-const port = process.env.PORT || 3000;
 
 function log(text) {
     console.log(kleur.cyan(text));
